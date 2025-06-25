87420962d8772f9a488fcc35ef18c028978cdbd1
Irina-Marti <iren.class@gmail.com>
2025-06-25 17:13:59 +0100

    add checking data input

--- Lesson3.1.py
+++ Lesson3.1.py
@@ -2,13 +2,18 @@ from datetime import datetime
 
 
 def get_days_from_today(date):
-    requested_date = datetime.strptime(date, "%Y-%m-%d").date()
-    date_today = datetime.today().date()
-    diff_days = (date_today - requested_date).days
+    try:
+        requested_date = datetime.strptime(date, "%Y-%m-%d").date()
+        date_today = datetime.today().date()
+        diff_days = (date_today - requested_date).days
+        return diff_days
 
-    return diff_days
+    except ValueError:
+        return "Date input in the wrong format. Please enter the date in this format: YYYY-MM-DD"
 
-input_date = "2020-10-09"
+
+
+input_date = "2020-10-06"
 day_difference = get_days_from_today(input_date)
 
 print("Today:", datetime.today().date())
